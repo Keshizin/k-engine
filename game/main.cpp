c8b25f62eabb0e41cf243b24b254ae6b19e27345
Keshizin <fab.takeshi@gmail.com>
2020-12-24 10:30:03 -0300

    Fixing CMakeLists.txt to link WIN32 properly

--- game/main.cpp
+++ game/main.cpp
@@ -1,6 +1,6 @@
 /*
 	Game Engine Template
-	This file provide a template for a game created with BPM Game Engine.
+	This file provide a template for a game created with K-Engine.
 
 	Copyright (C) 2020 Fabio Takeshi Ishikawa
 
@@ -24,14 +24,12 @@
 */
 
 #define WIN32_LEAN_AND_MEAN
-
 #include <windows.h>
+
 #include <iostream>
 #include <ge.h>
 
-
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	std::cout << "cmake test: " << test() << std::endl;
-	return 0;
+	return 1;
 }
